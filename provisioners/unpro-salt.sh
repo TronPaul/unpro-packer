961ffedc7507651b2c9e7f0083410c1f3f001a84
Mark McGuire <mark.b.mcg@gmail.com>
2015-03-08 23:34:27 -0500

    use more sudo

--- provisioners/unpro-salt.sh
+++ provisioners/unpro-salt.sh
@@ -4,7 +4,7 @@
 DIR=`mktemp -d`
 sudo wget -q -O $DIR/unpro-salt.tar.gz https://github.com/TronPaul/unpro-salt/archive/master.tar.gz
 tar -xzf $DIR/unpro-salt.tar.gz -C $DIR
-cp -r $DIR/unpro-salt-master/state /srv/salt
-cp -r $DIR/unpro-salt-master/pillar /srv/pillar
-salt-call state.highstate
+sudo cp -r $DIR/unpro-salt-master/state /srv/salt
+sudo cp -r $DIR/unpro-salt-master/pillar /srv/pillar
+sudo salt-call state.highstate
 rm -rf $DIR
