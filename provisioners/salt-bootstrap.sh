0c8d9708463779631c7530a1a834224c047a6291
Mark McGuire <mark.b.mcg@gmail.com>
2015-11-27 19:15:41 -0600

    upgrade to develop branch

--- provisioners/salt-bootstrap.sh
+++ provisioners/salt-bootstrap.sh
@@ -9,6 +9,6 @@ sudo mkdir /var/cache/salt
 sudo mkdir /var/run/salt
 
 curl -L https://raw.githubusercontent.com/saltstack/salt-bootstrap/stable/bootstrap-salt.sh -O bootstrap-salt.sh
-sudo sh bootstrap-salt.sh -P -X git v2015.8
+sudo sh bootstrap-salt.sh -P -X git develop
 
 sudo sed -i "s/^#master: salt$/master: salt-master.aws.teamunpro/g" /etc/salt/minion
